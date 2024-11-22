cdbb55cc8149b6b74a77d870180446d92f64ca3e
ahmedhoucine <ahmed.houcine@insat.ucar.tn>
2024-11-22 15:17:04 +0100

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CarteVisiteComponent } from './carte-visite/carte-visite.component';
+import { ColorChangerComponent } from './color-changer/color-changer.component';
+import { CvPlatformComponent } from './cv-platform/cv-platform.component';
+import { MiniWordComponent } from './mini-word/mini-word.component';
+import { RainbowComponent } from './rainbow/rainbow.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'carte_visite', component: CarteVisiteComponent },
+  { path: 'color_changer', component: ColorChangerComponent },
+  { path: 'cv_platforme', component: CvPlatformComponent },
+  { path: 'mini_word', component: MiniWordComponent },
+  { path: 'rainbow', component: RainbowComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
